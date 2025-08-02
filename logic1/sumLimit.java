e1aace15e6d3fb19fd174ab3351364c27338af80
musabirfan <22f23355@mec.edu.om>
2025-08-03 02:58:26 +0400

    Add logic1: sumLimit

---
+++ logic1/sumLimit.java
@@ -0,0 +1,14 @@
+public class sumLimit {
+    public int sumLimit(int a, int b) {
+        int sum = a+b;
+
+        String aStr = String.valueOf(a);
+        String sumStr = String.valueOf(sum);
+
+        if (sumStr.length() > aStr.length()) {
+            return a;
+        } else {
+            return sum;
+        }
+    }
+}
