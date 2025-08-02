ac94706170b5e305710d51e90ea1fedb64f13998
musabirfan <22f23355@mec.edu.om>
2025-08-03 02:57:54 +0400

    Add logic1: less20

---
+++ logic1/less20.java
@@ -0,0 +1,10 @@
+public class less20 {
+    public boolean less20(int n) {
+        if (n%20 == 20-1 || n%20 == 20-2){
+            return true;
+        }
+        else
+            return false;
+    }
+
+}
