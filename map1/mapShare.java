45ca93c265e5babd46dfa86355984058a463c317
musabirfan <22f23355@mec.edu.om>
2025-08-03 23:05:53 +0400

    Add Map1: mapShare

---
+++ map1/mapShare.java
@@ -0,0 +1,11 @@
+import java.util.Map;
+
+public class mapShare {
+    public Map<String, String> mapShare(Map<String, String> map) {
+        map.remove("c");
+        if (map.containsKey("a")){
+            map.put("b", map.get("a"));
+        }
+        return map;
+    }
+}
