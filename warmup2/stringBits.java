eb4140a4d432aea9f67adc964f7d48ac4552e29b
musabirfan <22f23355@mec.edu.om>
2025-08-03 20:41:42 +0400

    Add Warmup2: stringBits

---
+++ warmup2/stringBits.java
@@ -0,0 +1,10 @@
+public class stringBits {
+    public String stringBits(String str) {
+        String str2 = "";
+
+        for (int i=0; i<str.length(); i+=2){
+            str2 += str.charAt(i);
+        }
+        return str2;
+    }
+}
