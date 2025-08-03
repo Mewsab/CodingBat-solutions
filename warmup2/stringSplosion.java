cbe2121a159c5bcfb5055224ec4459b8527ea6a7
musabirfan <22f23355@mec.edu.om>
2025-08-03 20:50:23 +0400

    Add Warmup2: stringSplosion

---
+++ warmup2/stringSplosion.java
@@ -0,0 +1,10 @@
+public class stringSplosion {
+    public String stringSplosion(String str) {
+        String str2 = "";
+
+        for (int i=0; i<str.length(); i++){
+            str2 += str.substring(0, i+1);
+        }
+        return str2;
+    }
+}
